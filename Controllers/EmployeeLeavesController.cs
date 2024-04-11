cbe8cad20933c9ea19dee14cac1a9f00fea20490
roxzlir <emil.ch.nordin@gmail.com>
2024-04-11 20:51:53 +0200

    Started on EmployeeLeavesController

---
+++ Controllers/EmployeeLeavesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoxCorp.Data;
+
+namespace RoxCorp.Controllers
+{
+    public class EmployeeLeavesController : Controller
+    {
+        //Lägger med min kontext för att få kontakt med databasen
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeLeavesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        string selectedLeave = null;
+
+        public async Task<IActionResult> Index(int? leaveId)
+        {
+            var leaves = await _context.Leaves.ToListAsync();
+
+
+
+
+            return View();
+        }
+    }
+}
